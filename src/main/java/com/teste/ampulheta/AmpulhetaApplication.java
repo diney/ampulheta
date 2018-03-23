f43ca26cfcd1906e1e0e891faf241522522bc6d2
Diney Domingos de Souza <diney.dom@gmail.com>
2018-03-23 17:16:03 -0300

    Projeto criado

---
+++ src/main/java/com/teste/ampulheta/AmpulhetaApplication.java
@@ -0,0 +1,12 @@
+package com.teste.ampulheta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmpulhetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmpulhetaApplication.class, args);
+	}
+}
